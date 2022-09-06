3848bc3f5c242058939fb38c16d0ed7dbf19bd68
BERKAY İRVEM <REDKILL25@outlook.com>
2022-09-06 12:43:45 +0300

    Add files via upload

---
+++ Custom select box/app.js
@@ -0,0 +1,20 @@
+const select = document.querySelector(".select");
+const options_list = document.querySelector(".options-list");
+const options = document.querySelectorAll(".option");
+
+//show & hide options list
+select.addEventListener("click", () => {
+  options_list.classList.toggle("active");
+  select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+});
+
+//select option
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    options.forEach((option) => {option.classList.remove('selected')});
+    select.querySelector("span").innerHTML = option.innerHTML;
+    option.classList.add("selected");
+    options_list.classList.toggle("active");
+    select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+  });
+});
